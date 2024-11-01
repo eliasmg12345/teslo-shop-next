3c411e40d5d8bee86eef10eada3c2a28aec50fa9
eliasmg12345 <randyofgod666@gmail.com>
2024-11-01 16:23:13 -0400

    feat: auth validate form

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -13,10 +13,22 @@ export const authConfig: NextAuthConfig = {
         newUser: '/auth/new-account'
     },
     callbacks: {
+        authorized({ auth, request: { nextUrl } }) {
+            /* const isLoggedIn = !!auth?.user;
+            const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
+            if (isOnDashboard) {
+                if (isLoggedIn) return true;
+                return false; // Redirect unauthenticated users to login page
+            } else if (isLoggedIn) {
+                return Response.redirect(new URL('/dashboard', nextUrl));
+            } */
+            return true;
+        },
+        
         jwt({ token, user }) {
 
-            if(user){
-                token.data=user
+            if (user) {
+                token.data = user
             }
 
             return token
@@ -24,7 +36,7 @@ export const authConfig: NextAuthConfig = {
 
         session({ session, token, user }) {
             session.user = token.data as any
-            
+
             return session
         },
 
